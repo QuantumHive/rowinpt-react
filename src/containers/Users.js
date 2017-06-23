a8d9be09038eebaf49a958deec51d0225d525817
QuantumHive <QuantumHive@outlook.com>
2017-06-23 22:34:30 +0200

    searchbar for users

--- src/containers/Users.js
+++ src/containers/Users.js
@@ -4,19 +4,40 @@ import { connect } from 'react-redux';
 import UserRow from '../components/users/UserRow';
 
 class Users extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filterText: "",
+        };
+
+        this.handleSearchInputChange = this.handleSearchInputChange.bind(this);
+    }
+
+    handleSearchInputChange(event) {
+        const value = event.target.value.toLowerCase();
+        this.setState({
+            filterText: value
+        });
+    }
+
     render() {
+        const filter = this.state.filterText;
         return (
             <div className="col p-0">
                 <div className="input-group p-1">
                     <span className="input-group-addon">
                         <i className="fa fa-search" />
                     </span>
-                    <input type="text" className="form-control" placeholder="Zoeken" />
+                    <input type="text" className="form-control" placeholder="Zoeken" onChange={this.handleSearchInputChange} />
                 </div>
                 <div className="list-group">
                     {
-                        this.props.users.map(user =>
-                            <UserRow key={user.id} user={user} />)
+                        this.props.users.map(user => {
+                            const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
+                            if(fullName.indexOf(filter) === -1) return;
+                            return <UserRow key={user.id} user={user} />;
+                        })
                     }
                 </div>
             </div>
