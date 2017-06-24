3a1c68e044be55ed8d220849cac92e76b5e2f40b
QuantumHive <QuantumHive@outlook.com>
2017-06-24 17:08:35 +0200

    minor changes to profile jumbotron

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -14,15 +14,20 @@ class Profile extends React.Component {
             <div className="col p-0">
                 <div className="jumbotron jumbotron-fluid mb-3">
                     <div className="container-fluid">
-                        <div className="justify-content-start d-flex">
-                            <div>
+                        <div className="d-flex">
+                            <div className="text-center">
                                 {user.avatar === undefined ?
                                     <div className="avatar-circle">
                                         <span className="initials">{user.name[0]}</span>
                                     </div> :
                                     <img src={user.avatar} className="rounded-circle" width="100" height="100" />
                                 }
-                                <p className="lead text-center mt-3">{user.name}</p>
+                                <p className="lead mt-3">{user.name}</p>
+                            </div>
+                            <div className="ml-3 mt-1">
+                                <p><strong>Gewicht:</strong> 81 kg</p>
+                                <p><strong>Vetgehalte:</strong> 76%</p>
+                                <p><strong>Omvang:</strong> 88 cm</p>
                             </div>
                         </div>
                     </div>
