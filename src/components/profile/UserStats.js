ca4e157a75a9e5541d1ef6873e766e2520dc3f89
QuantumHive <QuantumHive@outlook.com>
2017-06-24 18:15:58 +0200

    included prototype of the doughnut chart for diets

--- src/components/profile/UserStats.js
+++ src/components/profile/UserStats.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Graph from './Graph';
 import moment from 'moment';
+import { Doughnut } from 'react-chartjs-2';
 
 const datasetsTemplate = {
     backgroundColor: 'rgba(255, 99, 132, 0.3)',
@@ -64,6 +65,44 @@ class UserStats extends React.Component {
                 <Graph data={this.state.weight} options={options} title={"GEWICHT"} subtitle={"Verlies"} badge={"14 kg"} />
                 <Graph data={this.state.fat} options={options} title={"VETGEHALTE"} subtitle={"Verlies"} badge={"24 %"} />
                 <Graph data={this.state.size} options={options} title={"OMVANG"} subtitle={"Verlies"} badge={"7 cm"} />
+
+                <div>
+                    <hr />
+                    <h3 className="mb-3">
+                        Voedingsschema
+                    </h3>
+                    <Doughnut data={{
+                        labels: [
+                            'KOOLHYDRATEN',
+                            'EIWITTEN',
+                            'VETTEN'
+                        ],
+                        datasets: [{
+                            data: [35, 40, 25],
+                            backgroundColor: [
+                                '#FF6384',
+                                '#36A2EB',
+                                '#FFCE56'
+                            ],
+                            hoverBackgroundColor: [
+                                '#FF6384',
+                                '#36A2EB',
+                                '#FFCE56'
+                            ],
+                        }],
+
+                    }} options={{
+                        legend: {
+                            position: 'left',
+                            labels: {
+                                fontSize: 14,
+                                boxWidth: 25
+                            }
+                        },
+                        cutoutPercentage: 65
+                    }} />
+                </div>
+
             </div>
         );
     }
