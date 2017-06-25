dda4ac00a056c3df04134235bc316962ecfa078a
QuantumHive <QuantumHive@outlook.com>
2017-06-25 23:25:50 +0200

    installed react-spinkit with bouncing spinner when users are loading

--- src/containers/Users.js
+++ src/containers/Users.js
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../actions/userAsyncActions';
 import UserRow from '../components/users/UserRow';
+import Spinner from 'react-spinkit';
 
 class Users extends React.Component {
     constructor(props) {
@@ -30,23 +31,29 @@ class Users extends React.Component {
     render() {
         const filter = this.state.filterText;
         return (
-            <div className="col p-0">
+            <div className="col p-0 d-flex flex-column">
                 <div className="input-group p-1">
                     <span className="input-group-addon">
                         <i className="fa fa-search" />
                     </span>
                     <input type="text" className="form-control" placeholder="Zoeken" onChange={this.handleSearchInputChange} />
                 </div>
-                { !this.props.isFetching ?
-                <div className="list-group">
-                    {
-                        this.props.users.map(user => {
-                            const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
-                            if (fullName.indexOf(filter) === -1) return;
-                            return <UserRow key={user.id} user={user} />;
-                        })
-                    }
-                </div> : <div className="mt-5 text-center">Loading...</div> }
+
+
+
+
+                {!this.props.isFetching ?
+                    <div className="list-group">
+                        {
+                            this.props.users.map(user => {
+                                const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
+                                if (fullName.indexOf(filter) === -1) return;
+                                return <UserRow key={user.id} user={user} />;
+                            })
+                        }
+                    </div> : <div className="d-flex justify-content-center col p-0">
+                        <Spinner className="align-self-center" name="double-bounce" fadeIn="half" style={{ width: "90px", height: "90px" }} />
+                    </div>}
             </div>
         );
     }
