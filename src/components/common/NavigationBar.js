5ca19011adaf733761e9b1f2d6294465695219c0
QuantumHive <QuantumHive@outlook.com>
2017-07-03 07:46:45 +0200

    disable profile until feature is implemented

--- src/components/common/NavigationBar.js
+++ src/components/common/NavigationBar.js
@@ -8,14 +8,14 @@ function NavigationBar(props) {
     const home = props.role === "User" ? (<li className="nav-item">
         <NavLink exact to={paths.default} className="nav-link" activeClassName="active"><i className="fa fa-home fa-2x" /></NavLink >
     </li>) : false;
-    const profile = props.role === "User" ? (<li className="nav-item">
-        <NavLink to={paths.Profile} className="nav-link" activeClassName="active"><i className="fa fa-user fa-2x" /></NavLink >
-    </li>) : false;
+    // const profile = props.role === "User" ? (<li className="nav-item">
+    //     <NavLink to={paths.Profile} className="nav-link" activeClassName="active"><i className="fa fa-user fa-2x" /></NavLink >
+    // </li>) : false;
     return (
         <nav className="navbar navbar-light bg-faded">
             <ul className="navbar-nav bg-faded nav-justified flex-row">
                 {home}
-                {profile}
+                {/*{profile}*/}
                 {props.role === "Admin" ?
                     <li className="nav-item">
                         <NavLink to={paths.UserSettings} className="nav-link" activeClassName="active"><i className="fa fa-cog fa-2x" /></NavLink >
